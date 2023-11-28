16c4bd7270e310b2d11978b9bcfa6179e1884047
Badrdineyk <badrdineyk9@gmail.com>
2023-11-28 07:42:39 -0800

    Add: 8-uppercase.py file

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def uppercase(str):
+    '''Prints a string in uppercase followed by a new line'''
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122
+            char_uppercase = chr(ord(c) - 32)
+        print("{}".format(char_uppercase), end='')
+    print("")
