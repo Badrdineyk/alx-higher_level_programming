cb17381618b43c31c71ca31b854238961dfa7798
Badrdineyk <badrdineyk9@gmail.com>
2024-01-19 09:04:22 -0800

    Add: 7-add_tuple.py file

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,18 @@
+#!/usr/bun/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    """adds 2 tuples."""
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 0:
+            tuple_a = 0, 0
+        else:
+            tuple_a = tuple_a[0], 0
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 0:
+            tuple_b = 0, 0
+        else:
+            tuple_b = tuple_b[0], 0
+
+    a, b = tuple_a
+    x, y = tuple_b
+
+    return a + x, b + y
