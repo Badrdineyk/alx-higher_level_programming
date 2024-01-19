e337698119909540c367610d4762bd2a62a72f03
Badrdineyk <badrdineyk9@gmail.com>
2024-01-19 04:42:52 -0800

    Add:

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    """ prints all integers of a list, in reverse order."""
+    if type(my_list) is list:
+        my_list.reverse()
+    for num in my_list:
+        print("{}".format(num))
