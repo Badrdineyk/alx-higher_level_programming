0f3cd9ab0fb364311b49c32431b88bb5eef80021
Badrdineyk <badrdineyk9@gmail.com>
2024-01-19 08:31:43 -0800

    Add: 6-print_matrix_integer.py file

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bun/python3
+def print_matrix_integer(matrix=[[]]):
+    """Prints a matrix of integers."""
+    for row in range(len(matrix)):
+        for i in range(len(matrix[row])):
+            print("{}".format(matrix[row][i]), end="")
+            if i != (len(matrix[row]) - 1):
+                print(" ", end="")
+        print("")
