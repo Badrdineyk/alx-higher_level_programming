ae1302014f25312bc24a55f257eada656392ef80
Badrdineyk <badrdineyk9@gmail.com>
2024-02-19 05:06:01 -0800

    Task 6 is solved

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a base geometry class"""
+
+
+class BaseGeometry:
+    """Represents base geometry"""
+    def area(self):
+        """Raises an Exception"""
+        raise Exception("area() is not implemented")
