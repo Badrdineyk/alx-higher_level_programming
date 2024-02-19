45395ce4c1112c477fdaddad1f0988726d988a7c
Badrdineyk <badrdineyk9@gmail.com>
2024-02-19 06:38:54 -0800

    Task 10 is solved

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -4,12 +4,14 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """Represent a square."""
+
     def __init__(self, size):
         """Initialize a new Square.
 
         Args:
             size (int): The size of the new Square.
         """
-        super().integer_validator("size", size)
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
