b298870d98f08664769c80cd4d76d179276b550d
Badrdineyk <badrdineyk9@gmail.com>
2024-02-19 04:57:46 -0800

    Task 4 is solved

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a class-checking function"""
+
+
+def inherits_from(obj, a_class):
+    """Checks if an object is an instance of a class
+    that inherited from the specified class.
+
+    args:
+        obj: The object to check
+        a_class: The class to be checked
+    Returns:
+        True if obj is an instance of a_class that inherited
+        from the specified class
+        Otherwise False
+    """
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
+        return True
+    return False
