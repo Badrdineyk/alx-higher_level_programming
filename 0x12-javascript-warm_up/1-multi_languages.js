b1398f2c4dc9bf487206c9abb862750dffbb20ca
Badrdineyk <badrdineyk9@gmail.com>
2024-06-18 19:12:39 +0000

    Update the file

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,2 @@
 #!/usr/bin/node
-const firstLine = 'C is fun';
-const secondLine = 'Python is cool';
-const thirdLine = 'Javascript is amazing';
-console.log(firstLine + '\n' + secondLine + '\n' + thirdLine);
+console.log('C is fun\nPython is cool\nJavascript is amazing');
