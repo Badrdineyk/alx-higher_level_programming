7e0eba5c71ae973d6a9318dc86ae4e1bcde30363
Badrdineyk <badrdineyk9@gmail.com>
2024-07-30 20:33:42 +0000

    Tasks solved

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,21 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error) {
-          console.log(JSON.parse(body).name);
-        }
-      });
+
+const req = require('request');
+const pathname = 'https://swapi-api.hbtn.io/api/films/';
+const slug = process.argv[2];
+req.get(pathname + slug, function (error, res, body) {
+  if (error) {
+    console.log(error);
+  }
+  const responseData = JSON.parse(body);
+  const characters = responseData.characters;
+  for (const i of characters) {
+    req.get(i, function (error, res, chars) {
+      if (error) {
+        console.log(error);
+      }
+      const mydata = JSON.parse(chars);
+      console.log(mydata.name);
     });
   }
 });
