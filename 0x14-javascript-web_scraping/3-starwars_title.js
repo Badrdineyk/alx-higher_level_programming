8c88d9ab5e4e43650766132fc8654c2497c3c63f
Badrdineyk <badrdineyk9@gmail.com>
2024-07-30 20:29:16 +0000

    Update the file

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
-let url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+
+const filmIndex = process.argv[2];
+const SWAPI_FILMS_URL = 'https://swapi-api.hbtn.io/api/films/';
+
+request(SWAPI_FILMS_URL + filmIndex, function (error, httpResponse, responseBody) {
+  if (error) {
+    console.log(error);
+  } else if (httpResponse.statusCode === 200) {
+    const filmData = JSON.parse(responseBody);
+    console.log(filmData.title);
+  } else {
+    console.log('Error code: ' + httpResponse.statusCode);
+  }
 });
